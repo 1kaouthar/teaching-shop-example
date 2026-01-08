a67c309bbedced71adf3837dc7994e341099fda7
unknown <kaouthar.asrar@gmail.com>
2026-01-08 17:09:07 +0100

    Add Review model linked to Product

--- backend/core/api/models.py
+++ backend/core/api/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -10,3 +11,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    author = models.CharField(max_length=100)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author} - {self.product.name}"
+
